cfd3de3946d8e91bc3d4091a1ea7470beb855e11
Prometheus1400 <prometheus1400@gmail.com>
2026-04-26 16:10:17 -0700

    Solve Project Euler problem 2

--- solutions/src/bin/p0002.rs
+++ solutions/src/bin/p0002.rs
@@ -1,5 +1,20 @@
-use lib::banner;
+use lib::prelude::run;
 
 fn main() {
-    println!("{}", banner(2));
+    run(2, || {
+        let mut p1 = 2; // represents previous value n
+        let mut n = 3; // current value
+        let mut ans = 2;
+
+        while n <= 4_000_000 {
+            if n % 2 == 0 {
+                ans += n;
+            }
+            let next_n = p1 + n;
+            p1 = n;
+            n = next_n;
+        }
+
+        ans
+    });
 }
