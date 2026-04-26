2fcf9c034bc4302a397376dbf9bd8b026c156c03
Prometheus1400 <prometheus1400@gmail.com>
2026-04-26 16:10:17 -0700

    Solve Project Euler problem 1

--- solutions/src/bin/p0001.rs
+++ solutions/src/bin/p0001.rs
@@ -1,5 +1,10 @@
-use lib::banner;
+use itertools::Itertools;
+use lib::prelude::run;
 
 fn main() {
-    println!("{}", banner(1));
+    run(1, || {
+        let n = 1000;
+        let ans: usize = (3..n).step_by(3).chain((5..n).step_by(5)).unique().sum();
+        ans
+    });
 }
